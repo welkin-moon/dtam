83d1dcdd4255c4aa8b950a5b3cc384641711a900
meteor <meteor_lab@outlook.com>
2026-08-12 20:42:38 +0800

    Remove merged 2.5 polish helper

--- build/patch-game-2.5-polish.cjs
+++
@@ -1,9 +0,0 @@
-const fs=require('fs');
-let src=fs.readFileSync('game.js','utf8');
-const re=/(if\(msg\.t==='vent_state'\)\{[\s\S]*?selfState\.ventId=String\(msg\.ventId\|\|''\);)/;
-const count=[...src.matchAll(new RegExp(re.source,'g'))].length;
-if(count!==1)throw new Error(`[2.5 polish] vent state expected 1 match, got ${count}`);
-src=src.replace(re,`$1selfState.ventReadyAt=Number(msg.ventReadyAt||0);selfState.ventExitAt=Number(msg.ventExitAt||0);`);
-new Function(src);
-fs.writeFileSync('game.js',src);
-console.log('[dtam] gameplay 2.5 polish applied');
