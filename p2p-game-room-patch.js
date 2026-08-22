ac277628fc88e0b8d19dd6f2eabbb04cac7c6945
meteor <meteor_lab@outlook.com>
2026-08-22 08:52:29 +0800

    fix(p2p): patch active hybrid GameRoom module

--- p2p-game-room-patch.js
+++ p2p-game-room-patch.js
@@ -1,4 +1,4 @@
-import { GameRoom } from './worker.js?v=p2p-browser-host-3';
+import { GameRoom } from './worker.js?v=p2p-browser-host-4';
 
 const originalWebSocketMessage = GameRoom.prototype.webSocketMessage;
 GameRoom.prototype.webSocketMessage = async function patchedBrowserHostMessage(ws, message) {
