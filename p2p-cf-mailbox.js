def7452cd2650fdad5393b6b0a00fe12de1d9385
meteor <meteor_lab@outlook.com>
2026-09-18 20:28:45 +0800

    Bump CF mailbox authority bundle

--- p2p-cf-mailbox.js
+++ p2p-cf-mailbox.js
@@ -1,4 +1,4 @@
-import { GameRoom } from './worker.js?v=p2p-browser-host-3';
+import { GameRoom } from './worker.js?v=20260918-archmusic1';
 
 const NativeWebSocket = window.WebSocket;
 const NativeRTCPeerConnection = window.RTCPeerConnection;
@@ -263,4 +263,4 @@ Object.defineProperties(P2PWebSocket.prototype,{CONNECTING:{value:0},OPEN:{value
 window.WebSocket=P2PWebSocket;
 setBadge('P2P · CF mailbox','自己的 Cloudflare 仅负责短时 SDP mailbox；无需 Durable Objects');
 console.log('DTAM experimental P2P transport',VERSION);
-await import('./game.js?v=20260918-v302session6-cf');
+await import('./game.js?v=20260918-archmusic1');
