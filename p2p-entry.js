20652735a6915d5d26527a28794288ae5997f25b
meteor <meteor_lab@outlook.com>
2026-09-19 13:51:27 +0800

    Advertise current capabilities in p2p-entry.js

--- p2p-entry.js
+++ p2p-entry.js
@@ -1,4 +1,4 @@
-import { GameRoom } from './worker.js?v=20260918-archmusic1';
+import { GameRoom } from './worker.js?v=20260919-servergate1';
 
 const NativeWebSocket = window.WebSocket;
 const NativeRTCPeerConnection = window.RTCPeerConnection;
@@ -267,7 +267,7 @@ class BrowserRoomHost {
     room.announceHostChange(previousHostId);
     authoritySocket.send(JSON.stringify({
       t: 'welcome', room: this.roomCode, resumed,
-      self: { id: player.id, token: player.token, name: player.name }, features: { bushVision: true, mapManifest: '/maps/east-beach-v1.json' }, hostId: room.hostId,
+      self: { id: player.id, token: player.token, name: player.name }, features: { bushVision: true, mapManifest: '/maps/east-beach-v1.json', musicSync: true, roomIdentityV2: true, p2pFallbackV2: true }, hostId: room.hostId,
       players: room.publicPlayers(), profiles: room.profiles(), voices: room.voiceDirectory(player),
       bodies: room.bodies, game: room.publicGame(player.id), selfState: room.selfState(player),
       p2p: true,
